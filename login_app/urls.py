2183c79c94751b45adc3dbd09ff58b00162d0255
Enrique Ibarra <rique.ibarra1@gmail.com>
2020-01-29 17:07:51 -0600

    fixing everything

--- login_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.index ),
-    path('user/register', views.register),
-    path('success', views.success),
-    path('user/login', views.user_login),
-    path('user/logout', views.user_logout),
-]
--- on_the_trail/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OnTheTrailConfig(AppConfig):
-    name = 'on_the_trail'
