97d5e0a6962e09656968c24895042fcd88d48ef6
Enrique Ibarra <rique.ibarra1@gmail.com>
2020-01-30 10:24:44 -0600

    some changes

--- on_the_trail/models.py
+++ on_the_trail/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from login_app.models import User
 
-# Create your models here.
+class Trail(models.Model):
+    trail_id = models.IntegerField()
+    user = models.ForeignKey(User, related_name = "favorite_trails", on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
