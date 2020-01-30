211477eae3abd6c4a9e96b01c12e6ec57aa5160e
Enrique Ibarra <rique.ibarra1@gmail.com>
2020-01-30 10:16:22 -0600

    Revert "added trail model"
    This reverts commit d57f560aba41353e0f7ee1e7cb116ce49d1a674c.

--- on_the_trail/models.py
+++ on_the_trail/models.py
@@ -1,8 +1,3 @@
 from django.db import models
-from login_app.models import User
 
-class Trail(models.Model):
-    trail_id = models.IntegerField()
-    user = models.ForeignKey(User, related_name = "favorite_trails", on_delete = models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+# Create your models here.
